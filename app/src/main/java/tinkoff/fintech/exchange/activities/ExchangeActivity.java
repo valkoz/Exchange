cc0a78bb16be3980a042df483da30cbcdfddabc2
valkoz <kozlov.valent@gmail.com>
2017-11-21 01:24:41 +0300

    Exchange check input validity and finish activity if success

--- app/src/main/java/tinkoff/fintech/exchange/activities/ExchangeActivity.java
+++ app/src/main/java/tinkoff/fintech/exchange/activities/ExchangeActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -50,12 +51,55 @@ public class ExchangeActivity extends AppCompatActivity {
         TextView textView1 = findViewById(R.id.second_currency);
         textView1.setText(to);
 
-        final List<RateObject> response = new ArrayList<>();
-
         RateCallback rateCallback = new RateCallback() {
             @Override
             public void onSuccess(RateObject rate) {
-                response.add(rate);
+
+                ed.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        String value = ed.getText().toString();
+                        if (Objects.equals(value, ""))
+                            tv.setText("");
+                        else {
+                            double mul = Double.parseDouble(value);
+                            double result = rate.getRate() * mul;
+                            tv.setText(Formatter.decimal(result));
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) { }
+                });
+
+                button.setOnClickListener(view -> {
+
+                    String value = ed.getText().toString();
+                    if (value.equals("")) {
+                        Toast.makeText(getBaseContext(), "Enter value", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        double mul = Double.parseDouble(value);
+                        if (mul > 0) {
+                            double result = rate.getRate() * mul;
+                            ExchangeOperation operation = new ExchangeOperation(
+                                    Calendar.getInstance().getTime(), from, to, mul, result);
+                            AsyncTask.execute(() -> AppDatabase.getAppDatabase(getApplicationContext())
+                                    .exchangeOperationDao()
+                                    .insertAll(operation));
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(getBaseContext(),
+                                    "Enter value above zero",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                });
             }
 
             @Override
@@ -68,34 +112,6 @@ public class ExchangeActivity extends AppCompatActivity {
         };
 
         RetrofitClient.sendRequest(rateCallback, from, to);
-
-        ed.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Log.i("TextWatcher", "onTextChanged");
-                double mul = Double.parseDouble(ed.getText().toString());
-                double result = response.get(0).getRate() * mul;
-                String res = Formatter.decimal(result);
-
-                tv.setText(res);
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) { }
-        });
-
-        button.setOnClickListener(view -> {
-            double mul = Double.parseDouble(ed.getText().toString());
-            double result = response.get(0).getRate() * mul;
-            ExchangeOperation operation = new ExchangeOperation(Calendar.getInstance().getTime(),
-                    from, to, mul, result);
-            AsyncTask.execute(() -> AppDatabase.getAppDatabase(getApplicationContext())
-                    .exchangeOperationDao()
-                    .insertAll(operation));
-        });
 
     }
 }
