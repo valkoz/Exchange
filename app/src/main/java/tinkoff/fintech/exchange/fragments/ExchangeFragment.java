5572e03adcb2c0e7a528caf6cf252c03ab8d9a4d
valkoz <kozlov.valent@gmail.com>
2017-11-22 21:42:03 +0300

    RecyclerView Basic impl without database save

--- app/src/main/java/tinkoff/fintech/exchange/fragments/ExchangeFragment.java
+++ app/src/main/java/tinkoff/fintech/exchange/fragments/ExchangeFragment.java
@@ -3,13 +3,17 @@ package tinkoff.fintech.exchange.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -20,7 +24,11 @@ import tinkoff.fintech.exchange.ExchangeListAdapter;
 import tinkoff.fintech.exchange.R;
 
 //TODO: FIXME Incorrect ListView state when download, adapter.notifyDataSetChanged() after init doesn't helps
-public class ExchangeFragment extends ListFragment {
+public class ExchangeFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public ExchangeFragment() {}
 
@@ -32,13 +40,19 @@ public class ExchangeFragment extends ListFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ArrayAdapter<Currency> adapter = null;
+        mRecyclerView = getView().findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
         try {
-            adapter = new ExchangeListAdapter(getActivity(), getModel());
+            List<Currency> currencies = getModel();
+            Collections.sort(currencies);
+            mAdapter = new MyAdapter(currencies);
+            mRecyclerView.setAdapter(mAdapter);
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
-        setListAdapter(adapter);
     }
 
     private List<Currency> getModel() throws ExecutionException, InterruptedException {
@@ -48,7 +62,7 @@ public class ExchangeFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.i("fragmentCreated", getClass().getCanonicalName());
+        //mRecyclerView = getView().findViewById(R.id.my_recycler_view);
         return inflater.inflate(R.layout.fragment_exchange, container, false);
     }
 
