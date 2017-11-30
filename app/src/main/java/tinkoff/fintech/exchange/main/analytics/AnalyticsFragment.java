c0627a987693fd6ac6e29a59705e10c37f53d12c
valkoz <kozlov.valent@gmail.com>
2017-11-30 12:29:41 +0300

    Migrate Analytics logic to ViewModel

--- app/src/main/java/tinkoff/fintech/exchange/main/analytics/AnalyticsFragment.java
+++ app/src/main/java/tinkoff/fintech/exchange/main/analytics/AnalyticsFragment.java
@@ -1,5 +1,7 @@
 package tinkoff.fintech.exchange.main.analytics;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -42,9 +44,8 @@ public class AnalyticsFragment extends ListFragment {
 
     RadioGroup radioGroup;
     GraphView graph;
-    RateWithDateCallback rateCallback;
-    List<GraphPoint> items = new ArrayList<>();
-    int responseCount = 7;
+    ArrayAdapter<String> adapter;
+    AnalyticsViewModel model;
 
     public AnalyticsFragment() {}
 
@@ -62,40 +63,11 @@ public class AnalyticsFragment extends ListFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initViews();
 
-        graph = getActivity().findViewById(R.id.graphView);
-        radioGroup = getActivity().findViewById(R.id.radioGroup);
-        initGraphStyle();
-
-        List<String> coins = AppDatabase.getAppDatabase(getContext()).currencyDao().getCurrencyNamesExcept(CurrencyName.EUR.name());
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                getActivity(), android.R.layout.simple_list_item_1, coins);
-        setListAdapter(adapter);
-
-
-        rateCallback = new RateWithDateCallback() {
-            @Override
-            public void onSuccess(RateObject rate, Date date) {
-                items.add(new GraphPoint(date, rate.getRate()));
-                if (items.size() == responseCount) {
-                    Collections.sort(items, new Comparator<GraphPoint>() {
-                        @Override
-                        public int compare(GraphPoint graphPoint, GraphPoint t1) {
-                            return  Float.compare(graphPoint.getX(), t1.getX());
-                        }
-                    });
-                    Log.i("Collection Items", items.toString());
-                    graph.setItems(items);
-                    graph.invalidate();
-                    items.clear();
-                }
-            }
-
-            @Override
-            public void onError(ErrorType type) {
-                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
-            }
-        };
+        model = ViewModelProviders.of(this).get(AnalyticsViewModel.class);
+        model.getCurrencyNames().observe(this, strings ->  { adapter.addAll(strings); });
+        model.getGraphPoints().observe(this, graphPoints -> { graph.setItems(graphPoints); });
 
     }
 
@@ -107,25 +79,26 @@ public class AnalyticsFragment extends ListFragment {
         switch (radioGroup.getCheckedRadioButtonId()) {
             case R.id.analytics_filter_week:
                 dates = CalendarIterator.getLast(Period.WEEK);
-                responseCount = dates.size();
                 break;
             case R.id.analytics_filter_two_weeks:
                 dates = CalendarIterator.getLast(Period.TWO_WEEKS);
-                responseCount = dates.size();
                 break;
             case R.id.analytics_filter_month:
                 dates = CalendarIterator.getLast(Period.MONTH);
-                responseCount = dates.size();
                 break;
             default:
                 break;
         }
+        model.loadGraph(dates, currencyName);
+    }
 
-        for (Date date : dates) {
-            items.clear();
-            RetrofitClient.getInstance()
-                    .sendRequestWithDate(rateCallback, Formatter.dateToString(date), currencyName);
-        }
+    private void initViews() {
+        graph = getActivity().findViewById(R.id.graphView);
+        radioGroup = getActivity().findViewById(R.id.radioGroup);
+        initGraphStyle();
+        adapter = new ArrayAdapter<>(
+                getActivity(), android.R.layout.simple_list_item_1, new ArrayList<>());
+        setListAdapter(adapter);
     }
 
     private void initGraphStyle() {
@@ -144,4 +117,5 @@ public class AnalyticsFragment extends ListFragment {
         graph.setLabelStep(5);
         graph.setScaleYLabelDateFormat(true);
     }
+
 }
---
+++ app/src/main/java/tinkoff/fintech/exchange/main/analytics/AnalyticsViewModel.java
@@ -0,0 +1,84 @@
+package tinkoff.fintech.exchange.main.analytics;
+
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.tinkoff.fintech.news.simpleplot.GraphPoint;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
+import tinkoff.fintech.exchange.enums.CurrencyName;
+import tinkoff.fintech.exchange.network.ErrorType;
+import tinkoff.fintech.exchange.network.RateObject;
+import tinkoff.fintech.exchange.network.RateWithDateCallback;
+import tinkoff.fintech.exchange.network.RetrofitClient;
+import tinkoff.fintech.exchange.util.AppDatabase;
+import tinkoff.fintech.exchange.util.Formatter;
+
+public class AnalyticsViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<String>> currencyNames;
+
+    private MutableLiveData<List<GraphPoint>> graphPoints;
+
+
+    public AnalyticsViewModel(@NonNull Application application) {
+        super(application);
+        currencyNames = new MutableLiveData<List<String>>();
+        graphPoints = new MutableLiveData<List<GraphPoint>>();
+        currencyNames.postValue(getFromDb());
+    }
+
+    private List<String> getFromDb() {
+         return AppDatabase.getAppDatabase(getApplication()).currencyDao().getCurrencyNamesExcept(CurrencyName.EUR.name());
+    }
+
+    public LiveData<List<String>> getCurrencyNames() {
+        return currencyNames;
+    }
+
+    public LiveData<List<GraphPoint>> getGraphPoints() {
+        return graphPoints;
+    }
+
+    public void loadGraph(List<Date> dates, String currencyName) {
+
+        List<GraphPoint> items = new ArrayList<>();
+
+        RateWithDateCallback rateCallback = new RateWithDateCallback() {
+
+            @Override
+            public void onSuccess(RateObject rate, Date date) {
+                items.add(new GraphPoint(date, rate.getRate()));
+                if (items.size() == dates.size()) {
+                    Collections.sort(items, new Comparator<GraphPoint>() {
+                        @Override
+                        public int compare(GraphPoint graphPoint, GraphPoint t1) {
+                            return  Float.compare(graphPoint.getX(), t1.getX());
+                        }
+                    });
+                    graphPoints.postValue(items);
+                }
+            }
+
+            @Override
+            public void onError(ErrorType type) { }
+        };
+
+        for (Date date : dates) {
+            items.clear();
+            RetrofitClient.getInstance()
+                    .sendRequestWithDate(rateCallback, Formatter.dateToString(date), currencyName);
+        }
+    }
+}
